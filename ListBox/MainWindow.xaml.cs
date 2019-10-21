e5827518f8b5d12a68a1a11a1480ac423c64cbe8
Paula-18 <paunahappy@gmail.com>
2019-10-21 14:45:16 -0500

    Agregar archivos de proyecto.

---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace ListBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> Colores = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Colores.Add("rojo");
+            Colores.Add("naranja");
+            Colores.Add("amarrillo");
+            Colores.Add("verde");
+
+            lstColores.ItemsSource = Colores;
+            
+        }
+
+        private void btnNuevoColor_Click(object sender, RoutedEventArgs e)
+        {
+            Colores.Add(txtbox1.Text);
+            txtbox1.Text = "";
+
+        }
+    }
+}
